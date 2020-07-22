6f1dfbb90c6ef6a8a20d86c592b3152f54e566fa
davidgeorgebell <bell.dave93@gmail.com>
2020-07-22 16:14:26 +0800

    Global styles, homepage info added

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,27 @@ export default function Home() {
   return (
     <div className='container'>
       <Head>
-        <title>Create Next App</title>
+        <title>Traditional Barber Shop</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
+      <nav>
+        <ul>
+          <li>TESTIMONIALS</li>
+          <li>CONTACT</li>
+          <li>LOGO</li>
+          <li>GALLERY</li>
+          <li>ABOUT US</li>
+        </ul>
+      </nav>
       <main>
-        <h1 className='title'>Jackson Brothers Barber Shop</h1>
+        <div>
+          <h1 className='title'>Jackson Brothers Barber Shop</h1>
+          <p>New barbershop using old school methods for cutting mens hair</p>
+          <div>
+            <a>BOOK NOW</a>
+          </div>
+        </div>
+        <div>IMAGE GOES HERE</div>
       </main>
     </div>
   );
