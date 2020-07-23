9b27dd0fe7b2f6e148df95020bca83390c5c8ecc
davidgeorgebell <bell.dave93@gmail.com>
2020-07-23 17:35:48 +0800

    styling

--- pages/index.js
+++ pages/index.js
@@ -2,36 +2,30 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto min-h-screen bg-teal-900'>
       <Head>
         <title>Traditional Barber Shop</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <header className=' items-center justify-between max-w-4xl mx-auto p-4 md:p-8'>
-        <nav>
-          <ul className='flex justify-between'>
-            <li>TESTIMONIALS</li>
-            <li>CONTACT</li>
-            <li>LOGO</li>
-            <li>GALLERY</li>
-            <li>ABOUT US</li>
-          </ul>
+      <header className='flex justify-center pb-8'>
+        <nav className='pt-4'>
+          <span>Logo</span>
         </nav>
       </header>
       <main>
-        <div className='main-grid   '>
-          <h1 className='main-title text-2xl md:text-5xl lg:text-5xl xl:text-5xl font-bold'>
+        <div className='main-grid'>
+          <h1 className='main-title text-3xl md:text-5xl lg:text-5xl xl:text-5xl font-bold text-white'>
             JACKSON BROTHERS BARBERSHOP
           </h1>
-          <div className='main-image   mt-8'>
+          <div className='main-image mt-4'>
             <img className='rounded-lg shadow-xl ' src='/barber-1.jpg' />
           </div>
           <div className='bits'>
-            <p className='main-intro text-lg font-serif mb-8'>
+            <p className='main-intro text-lg font-serif mb-8 text-grey-800'>
               New barbershop using old school methods for cutting mens hair
             </p>
             <div className='main-btn-wrapper mb-8'>
-              <button className='bg-orange-600 hover:bg-orange-700 text-white  py-2 px-4 rounded'>
+              <button className='bg-yellow-600 hover:bg-yellow-700 text-gray-900  py-2 px-4 rounded shadow-xl'>
                 <a>BOOK NOW</a>
               </button>
             </div>
