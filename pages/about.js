fe8bd756748612840b9a00f52548eb065a3c79cb
davidgeorgebell <bell.dave93@gmail.com>
2020-08-02 16:28:40 +0800

    tweaks

--- pages/about.js
+++ pages/about.js
@@ -7,12 +7,12 @@ export default function about() {
       <div className='p-4 container mx-auto max-w-2xl mt-10'>
         <div className=' p-6 pb-10 shadow-lg rounded-lg'>
           <div className='flex flex-col justify center'>
-            <h1 className='text-6xl font-serif text-white mb-10'>About</h1>
-            <p className='text-gray-200 text-2xl pb-2'>
+            <h1 className='text-2xl font-serif text-white mb-10'>About</h1>
+            <p className='text-gray-200 text-xl pb-2'>
               This is an example of a streamline website for a business built by{' '}
               <span className='text-yellow-500'>David Bell.</span>
             </p>
-            <p className='text-gray-200 text-2xl'>
+            <p className='text-gray-200 text-xl'>
               If you would like a website for your business you can contact him
               here{' '}
               <a
