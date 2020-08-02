acd7e421f9c8855719e277ea3504d4829f787e16
davidgeorgebell <bell.dave93@gmail.com>
2020-08-02 16:27:34 +0800

    Read me

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,16 +4,16 @@ import Link from 'next/link';
 export default function NavBar() {
   return (
     <nav className='min-w-full'>
-      <ul className='flex items-center justify-between max-w-2xl mx-auto text-gray-200 px-4'>
+      <ul className='flex items-center justify-around max-w-2xl mx-auto text-gray-200 px-4'>
         <li>
           <Link href='/open'>
-            <a className='hover:underline'>OPENING TIMES</a>
+            <a className='hover:underline text-xs md:text-lg'>TIMES</a>
           </Link>
         </li>
         <li>
           <Link href='/'>
             <a>
-              <h1 className='text-5xl font-serif text-yellow-500 font-bold'>
+              <h1 className='text-2xl md:text-5xl font-serif text-yellow-500 font-bold '>
                 JB
               </h1>
             </a>
@@ -21,7 +21,7 @@ export default function NavBar() {
         </li>
         <li>
           <Link href='/about'>
-            <a className='hover:underline'>ABOUT</a>
+            <a className='hover:underline text-xs md:text-lg'>ABOUT</a>
           </Link>
         </li>
       </ul>
--- pages/open.js
+++ pages/open.js
@@ -5,7 +5,7 @@ import { AnimationX } from '../components/AnimationX';
 export default function open() {
   return (
     <Layout>
-      <div className='flex flex-col justify-center container  px-2 lg:max-w-xl mx-auto'>
+      <div className='flex flex-col justify-center container px-6  md:px-2 lg:max-w-xl mx-auto'>
         <AnimationX>
           <div className='grid grid-cols-2 gap-10 py-10 mx-auto'>
             <ul className='text-white'>
@@ -17,7 +17,8 @@ export default function open() {
               <li className='border-t-2 border-gray-500 font-medium'>Sat</li>
               <li className='border-t-2 border-gray-500 font-medium'>Sun</li>
             </ul>
-            <ul className='text-white '>
+
+            <ul className='text-white text-right'>
               <li className=' text-yellow-500'>8.00am - 5.00pm</li>
               <li className=' text-yellow-500'>8.00am - 5.00pm</li>
               <li className=' text-yellow-500'>8.00am - 5.00pm</li>
