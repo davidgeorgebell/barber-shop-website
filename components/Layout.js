81198d9a23f81e3cdb33f9e78440fab450b504fb
davidgeorgebell <bell.dave93@gmail.com>
2020-08-02 16:36:32 +0800

    tweaks

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ import NavBar from './NavBar.js';
 export default function Layout({ children }) {
   return (
     <div className='min-h-screen min-w-screen bg-teal-900'>
-      <header className='flex justify-center pb-5'>
+      <header className='flex justify-center pb-5 md:pb-12'>
         <NavBar />
       </header>
       <main>{children}</main>
